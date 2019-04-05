7c11f75ffee5881c3cf8c0dc4f60cd531cb88dfd
andrey_velsk <andrey_velsk@mail.ru>
2019-04-05 11:46:46 +0300

    wpquery initial

--- category.php
+++ category.php
@@ -17,50 +17,24 @@
             <div class="col-md-8">
                 <div class="content-text">
                     <?php
-                    
-                        global $post;
-                        $category = get_category( get_query_var( 'cat' ) );
-                        $args = array( 'posts_per_page' => 2, 'offset'=> 0, 'category' => $category->cat_ID );
-                        $myposts = get_posts( $args );
-                        if (count($myposts)){
-                            foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-                                <div class="post">
-                                        <div class="post-image">
-                                            <img src="img/main.jpg" alt="">
-                                        </div>
-                                        <div class="post-info">
-                                            <h4>
-                                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-                                            </h4>
-                                            <div class="post-text">
-                                                <?php
-                                                    the_excerpt();
-                                                ?>
-                                            </div>
-                                            <div class="post-meta">
-                                                <a href="<?php the_permalink(); ?>">
-                                                    <button class="button-main">
-                                                        Подробнее
-                                                    </button>
-                                                </a>
-                                                <span>
-                                                    <?php the_date('d-m-Y H:i'); ?>
-                                                </span>
-                                            </div>
-                                        </div>
-                                </div>
-                                    <!--end post -->
-                            <?php endforeach; 
+                        global $wp_query;
+
+                        $save_wpq = $wp_query;
+                        
+                        // ваш запрос и код вывода с пагинацией
+                        $wp_query = new WP_Query( $args );
+                        while ( $wp_query->have_posts() ) {
+                            $wp_query->the_post();
+                        
+                            echo 1;
                         }
-                        else{
-                        ?>
-                            <div class="subinfo">
-                                В данной категории нет записей
-                            </div>
-                        <?php
-                        }
-                    wp_reset_postdata();?>
-                    
+                        
+                        // пагинация
+                        
+                        // вернем global $wp_query
+                        wp_reset_postdata();
+                        $wp_query = $save_wpq;
+                    ?>
                 </div>
             </div>
             <!--/content-->
