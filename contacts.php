318df9e167b218b37cd512c4bc76d6c91edc2877
andrey_velsk <andrey_velsk@mail.ru>
2019-04-11 12:53:50 +0300

    РѕР±СЂР°С‚РЅР°СЏ СЃРІСЏР·СЊ С‡РµСЂРµР· РєРѕРјРјРµРЅС‚Р°СЂРёРё

--- contacts.php
+++ contacts.php
@@ -27,26 +27,76 @@ Template Name: Контакты
                     <p class="subinfo">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam perspiciatis corporis consequatur laborum pariatur magnam, sint asperiores amet quaerat ipsa non aliquam dicta, fugit suscipit ratione eum accusantium sapiente repellat.
                     </p>
+                
+                    <?php
+                        $commenter = wp_get_current_commenter();
+                        $fields = array(
+                            'author' => '
+                                <div class="col-md-6 contactform-row">
+                                    <input id="author" name="author" class="contactform-input" type="text" 
+                                    value="' . esc_attr( $commenter['comment_author'] ) . '" 
+                                    placeholder="Имя">
+                                </div>
+                            ',
+                            'email' => '
+                                <div class="col-md-6 contactform-row">
+                                <input id="email" name="email" class="contactform-input" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' placeholder="E-mail"/>
+                                </div>
+                            '
+                        );
 
-                    <form action="<?php echo site_url() ?>/send.php" method="POST" class="contactform">
-                        <div class="row">
-                            <div class="col-md-6 contactform-row">
-                                <input name="name" class="contactform-input" type="text" placeholder="Имя">
-                            </div>
-                            <div class="col-md-6 contactform-row">
-                                <input name="phone" class="contactform-input" type="text" placeholder="Телефон">
-                            </div>
-                            <div class="col-md-12 contactform-row">
-                                <textarea name="message" class="contactform-input" name="" id="" cols="30" rows="10" placeholder="Сообщение"></textarea>
-                            </div>
+                        $args = array(
+                            'fields' => $fields,
+                            'title_reply' => '',
+                            'logged_in_as' => '
+                                <div class="col-md-12 contactform-row contactform-logedinas">' 
+                                    . sprintf( __( 'Вы вошли как <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Выйти?</a>' ), admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '
+                                </div>
+                            ',
+                            'comment_notes_after' => '',
+                            'comment_notes_before' => '',
+                            'comment_field' => 
+                                '
+                                <p class="col-md-12 contactform-row">
+                                    <textarea name="comment" class="contactform-input" id="comment" cols="30" rows="10" placeholder="Сообщение"></textarea>
+                                </p>
+                                ',
+                            'submit_field' => 
+                                '
+                                <p class="form-submit row">
+                                    <div class="col-md-12">
+                                    %1$s %2$s
+                                    </div>
+                                </p>
+                                ',
+                            'label_submit'=>'отправить',
+                            'class_form' => 'contactform row',
+                            'class_submit' => 'button-main'
                             
-                            <div class="col-md-12">
-                                <button class="button-main">
-                                    Отправить
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        );
+                        remove_action( 'set_comment_cookies', 'wp_set_comment_cookies' );
+                        add_filter('comment_form_fields', 'kama_reorder_comment_fields' );
+                        function kama_reorder_comment_fields( $fields ){
+                            // die(print_r( $fields )); // посмотрим какие поля есть
+
+                            $new_fields = array(); // сюда соберем поля в новом порядке
+
+                            $myorder = array('author','email', 'comment'); // нужный порядок
+
+                            foreach( $myorder as $key ){
+                                $new_fields[ $key ] = $fields[ $key ];
+                                unset( $fields[ $key ] );
+                            }
+
+                            // если остались еще какие-то поля добавим их в конец
+                            if( $fields )
+                                foreach( $fields as $key => $val )
+                                    $new_fields[ $key ] = $val;
+
+                            return $new_fields;
+                        }
+                        comment_form( $args );
+                    ?>
                 </div>
             </div>
             <!--/content-->
